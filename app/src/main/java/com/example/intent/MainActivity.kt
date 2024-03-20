8565c86444aab30f3d647ac16e18e5036f2a8db0
Brun0HC <bruno.cobuci@biotronica.tech>
2024-03-20 11:49:28 -0300

    Novo jeito de fazer intent

--- app/src/main/java/com/example/intent/MainActivity.kt
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.intent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.intent.Constantes.PARAMETRO_EXTRA
-import com.example.intent.Constantes.PARAMETRO_REQUEST_CODE
 import com.example.intent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy{
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private lateinit var parl: ActivityResultLauncher<Intent> // parametro activity result launcher (parl)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
@@ -22,22 +29,51 @@ class MainActivity : AppCompatActivity() {
         }
 
         amb.entrarParametroBt.setOnClickListener {
+            // INTENT EXPLÍCITA
             Intent(this, ParametroActivity::class.java).also{
                 it.putExtra(PARAMETRO_EXTRA, amb.parametroTv.text.toString()) // chave e valor -> envia o valor do text view pro campo de input na outra tela
-                startActivityForResult(it, PARAMETRO_REQUEST_CODE)
+                parl.launch(it)
             }
         }
+
+        parl = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            object: ActivityResultCallback<ActivityResult> {
+                override fun onActivityResult(result: ActivityResult) {
+                    if(result.resultCode == RESULT_OK){
+                        result.data?.getStringExtra(PARAMETRO_EXTRA)?.let{
+                            amb.parametroTv.text = it
+                        }
+                    }
+                }
+            })
+
+        //TEM UM DESSE PRA CADA TELA
+//        parl = registerForActivityResult(
+//            ActivityResultContracts.StartActivityForResult()) // contrato
+//        { result -> // lambda
+//            if (result.resultCode == RESULT_OK) {
+//                result.data?.getStringExtra(PARAMETRO_EXTRA)?.let {
+//                    amb.parametroTv.text = it
+//                }
+//            }
+//        }
+
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?):Boolean{
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == PARAMETRO_REQUEST_CODE){
-            if(resultCode == RESULT_OK){
-                if(data != null){
-                    val parametro = data.getStringExtra(PARAMETRO_EXTRA)
-                    amb.parametroTv.text = parametro
-                }
-            }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean{
+        return when(item.itemId){
+            R.id.viewMi -> {true}
+            R.id.callMi -> {true}
+            R.id.dialMi -> {true}
+            R.id.pickMi -> {true}
+            R.id.chooserMi -> {true}
+            else -> {false}
         }
     }
 }
